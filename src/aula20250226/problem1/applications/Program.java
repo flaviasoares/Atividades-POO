d7d26254a8093017dab4d1b45297ac1e92d0790a
flaviasoares <flaviasoares@ic.ufal.br>
2025-02-27 21:59:11 -0300

    Problem 2, proposed in class, has been started but it needs some more testing.

--- src/aula20250226/problem1/applications/Program.java
+++ src/aula20250226/problem1/applications/Program.java
@@ -4,7 +4,6 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Scanner;
-
 import aula20250226.problem1.entities.Reservation;
 
 public class Program {
